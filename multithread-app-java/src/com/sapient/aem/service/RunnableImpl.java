77f2bb60bb8cda308c5d2602a9b505801f41b883
Reddydeepa1 <deepareddyp281@gmail.com>
2022-03-04 13:32:07 +0530

    third commit

---
+++ multithread-app-java/src/com/sapient/aem/service/RunnableImpl.java
@@ -0,0 +1,13 @@
+package com.sapient.aem.service;
+/*
+ * Implementation class of Runnable interface
+ */
+public class RunnableImpl implements Runnable{
+
+	@Override
+	public void run() {
+		//returns current Thread object
+		System.out.println(Thread.currentThread());
+		System.out.println("End of run() method");		
+	}
+}
